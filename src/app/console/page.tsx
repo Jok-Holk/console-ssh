73c1b63daa7170b18c336b5531807c66c1141991
Jok-Holk <jokholk.dev@gmail.com>
2025-09-24 23:47:38 +0700

    test

--- src/app/console/page.tsx
+++ src/app/console/page.tsx
@@ -41,7 +41,7 @@ export default function ConsolePage() {
       term.current.focus();
 
       const token = document.cookie.split("authToken=")[1]?.split(";")[0];
-      socket.current = io("/socket.io/t", { auth: { token } });
+      socket.current = io("/socket.io/", { auth: { token } });
 
       socket.current.on("connect", () => {
         console.log("Socket connected");
