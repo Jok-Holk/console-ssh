5d47d96b1972882c2ff3c4bf742ae2440b17dedd
Jok-Holk <jokholk.dev@gmail.com>
2025-09-25 01:20:00 +0700

    test

--- socket-server.ts
+++ socket-server.ts
@@ -3,11 +3,13 @@ import { createServer } from "https";
 import { Server as SocketServer } from "socket.io";
 import { readFileSync } from "fs";
 import { Client } from "ssh2";
+
 const port = 3001;
 const httpsServer = createServer({
   cert: readFileSync("/etc/letsencrypt/live/console.jokholk.dev/fullchain.pem"),
   key: readFileSync("/etc/letsencrypt/live/console.jokholk.dev/privkey.pem"),
 });
+
 const io = new SocketServer(httpsServer, {
   cors: {
     origin: ["https://console.jokholk.dev", "http://localhost:3000"],
@@ -16,16 +18,22 @@ const io = new SocketServer(httpsServer, {
   },
   path: "/socket.io/",
 });
+
 io.on("connection", (socket) => {
   const ssh = new Client();
   ssh
     .on("ready", () => {
       ssh.shell(
-        { term: "xterm-256color", rows: 24, cols: 80 },
+        {
+          term: "xterm-256color",
+          rows: 24,
+          cols: 80,
+          modes: {
+            ECHO: 0,
+          },
+        },
         (err: any, stream: any) => {
           if (err) return socket.disconnect();
-          // Disable echo immediately
-          stream.write("stty -echo\r\n");
           stream.on("close", () => ssh.end());
           stream.on("data", (data: Buffer) => {
             socket.emit("output", data.toString());
@@ -52,6 +60,7 @@ io.on("connection", (socket) => {
     });
   socket.on("disconnect", () => ssh.end());
 });
+
 httpsServer.listen(port, "0.0.0.0", () =>
   console.log(`Socket.io on https://localhost:${port}`)
 );
