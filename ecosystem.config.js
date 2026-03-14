bfc38f1d91b9bbe8523e99b7f523631e3d2a2e96
Jok-Holk <jokholk.dev@gmail.com>
2026-03-14 20:01:26 +0700

    up ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,19 +5,22 @@ module.exports = {
       script: "node_modules/.bin/next",
       args: "start",
       cwd: "/root/console-ssh",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
+      env: { NODE_ENV: "production", PORT: 3000 },
+      max_memory_restart: "400M",
     },
     {
       name: "socket-server",
-      script: "socket-server.ts",
-      interpreter: "node_modules/.bin/tsx",
+      script: "./socket-server.ts",
+      interpreter: "node_modules/.bin/ts-node",
       cwd: "/root/console-ssh",
-      env: {
-        NODE_ENV: "production",
-      },
+      env: { NODE_ENV: "production" },
+    },
+    {
+      name: "cv-service",
+      script: "./dist/server/entry.mjs",
+      cwd: "/root/console-ssh/cv-service",
+      env: { NODE_ENV: "production", HOST: "0.0.0.0", PORT: 4321 },
+      max_memory_restart: "300M",
     },
   ],
 };
